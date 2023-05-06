890d893e6079a0a06e573d22c0e032cd8afacc2f
Mitra Sahatoo <mitra.sahatoo@gmail.com>
2023-05-06 13:09:32 -0600

    created grid

---
+++ src/routes/Game.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import Grid from './Grid.svelte';
+  import { levels, type Level } from './level';
+
+  const level = levels[0];
+
+  let size: number = level.size;
+  let grid: string[] = create_grid(level);
+  let found: string[] = [];
+
+  function create_grid(level: Level): string[] {
+    const copy = level.emojis.slice();
+    const pairs: string[] = [];
+
+    for (let i = 0; i < level.size ** 2 / 2; i++) {
+      const index = Math.floor(Math.random() * copy.length);
+      const emoji = copy[index];
+
+      copy.splice(index, 1);
+      pairs.push(emoji);
+    }
+
+    pairs.push(...pairs);
+    return pairs;
+  }
+</script>
+
+<div class="game">
+  <div class="info">timer</div>
+  <div class="grid-container">
+    <Grid
+      {grid}
+      on:found={(e) => {
+        found = [...found, e.detail.emoji];
+      }}
+      {found}
+    />
+  </div>
+  <div class="info">icons</div>
+</div>
+
+<style>
+  .game {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: min(1vmin, 0.5rem);
+  }
+
+  .info {
+    width: 80em;
+    height: 10em;
+    background: purple;
+  }
+
+  .grid-container {
+    width: 80em;
+    height: 80em;
+    background: teal;
+  }
+</style>
---
+++ src/routes/Square.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let emoji: string;
+  export let selected: boolean;
+  export let found: boolean;
+</script>
+
+<div class="square" class:flipped={selected}>
+  <button on:click />
+  {#if !found}
+    <span>{emoji}</span>
+  {/if}
+</div>
+
+<style>
+  .square {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: yellow;
+  }
+
+  button {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+  .flipped button {
+    background: red;
+  }
+
+  span {
+    font-size: 5em;
+    pointer-events: none;
+  }
+</style>
