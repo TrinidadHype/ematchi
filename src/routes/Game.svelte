a7bd3a2627359b4a64e982a65ed5c18b165b0498
Mitra Sahatoo <mitra.sahatoo@gmail.com>
2023-05-06 20:23:42 -0600

    game logic

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,63 +1,117 @@
 <script lang="ts">
-  import Grid from './Grid.svelte';
-  import { levels, type Level } from './level';
+    import { createEventDispatcher, onMount } from "svelte";
+    import Countdown from "./Countdown.svelte";
+    import Found from "./Found.svelte";
+    import Grid from "./Grid.svelte";
+    import type { Level } from "./level";
+    import { shuffle } from "./utils";
 
-  const level = levels[0];
+    const dispatch = createEventDispatcher();
 
-  let size: number = level.size;
-  let grid: string[] = create_grid(level);
-  let found: string[] = [];
+    let size: number;
+    let grid: string[] = [];
+    let found: string[] = [];
+    let remaining: number = 0;
+    let duration: number = 0;
+    let playing: boolean = false;
 
-  function create_grid(level: Level): string[] {
-    const copy = level.emojis.slice();
-    const pairs: string[] = [];
+    export function start(level: Level) {
+        size = level.size;
+        grid = create_grid(level);
+        remaining = duration = level.duration;
 
-    for (let i = 0; i < level.size ** 2 / 2; i++) {
-      const index = Math.floor(Math.random() * copy.length);
-      const emoji = copy[index];
-
-      copy.splice(index, 1);
-      pairs.push(emoji);
+        resume();
     }
 
-    pairs.push(...pairs);
-    return pairs;
-  }
+    function resume() {
+        playing = true;
+        countdown();
+
+        dispatch("play");
+    }
+
+    function create_grid(level: Level): string[] {
+        const copy = level.emojis.slice();
+        const pairs: string[] = [];
+
+        for (let i = 0; i < level.size ** 2 / 2; i++) {
+            const index = Math.floor(Math.random() * copy.length);
+            const emoji = copy[index];
+
+            copy.splice(index, 1);
+            pairs.push(emoji);
+        }
+
+        pairs.push(...pairs);
+        return shuffle(pairs);
+    }
+
+    function countdown() {
+        const start = Date.now();
+        let remaining_at_start = remaining;
+
+        function loop() {
+            if (!playing) return;
+            requestAnimationFrame(loop);
+
+            remaining = remaining_at_start - (Date.now() - start);
+
+            if (remaining <= 0) {
+                dispatch("lose");
+                playing = false;
+            }
+        }
+
+        loop();
+    }
 </script>
 
-<div class="game">
-  <div class="info">timer</div>
-  <div class="grid-container">
-    <Grid
-      {grid}
-      on:found={(e) => {
-        found = [...found, e.detail.emoji];
-      }}
-      {found}
-    />
-  </div>
-  <div class="info">icons</div>
+<div class="game" style="--size: {size}">
+    <div class="info">
+        {#if playing}
+            <Countdown
+                {remaining}
+                {duration}
+                on:click={() => {
+                    playing = false;
+                    dispatch("pause");
+                }}
+            />
+        {/if}
+    </div>
+    <div class="grid-container">
+        <Grid
+            {grid}
+            on:found={(e) => {
+                found = [...found, e.detail.emoji];
+
+                if (found.length === (size * size) / 2) {
+                    dispatch("win");
+                }
+            }}
+            {found}
+        />
+    </div>
+    <div class="info"><Found {found} /></div>
 </div>
 
 <style>
-  .game {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    font-size: min(1vmin, 0.5rem);
-  }
+    .game {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        font-size: min(1vmin, 0.5rem);
+    }
 
-  .info {
-    width: 80em;
-    height: 10em;
-    background: purple;
-  }
+    .info {
+        width: 80em;
+        height: 10em;
+    }
 
-  .grid-container {
-    width: 80em;
-    height: 80em;
-    background: teal;
-  }
+    .grid-container {
+        width: 80em;
+        height: 80em;
+    }
 </style>
--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,51 +1,53 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
-  import Square from './Square.svelte';
+    import { createEventDispatcher } from "svelte";
+    import Square from "./Square.svelte";
 
-  export let grid: string[];
-  export let found: string[];
+    export let grid: string[];
+    export let found: string[];
 
-  const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher();
 
-  let a = -1;
-  let b = -1;
-  let reset_timeout: number;
+    let a = -1;
+    let b = -1;
+    let reset_timeout: number;
 </script>
 
 <div class="grid">
-  {#each grid as emoji, i}
-    <Square
-      {emoji}
-      selected={a === i || b === i}
-      found={found.includes(emoji)}
-      on:click={() => {
-        clearTimeout(reset_timeout);
-        if (a === -1 && b === -1) {
-          a = i;
-        } else if (b === -1) {
-          b = i;
-          if (grid[a] === grid[b]) {
-            dispatch('found', { emoji });
-          } else {
-            reset_timeout = setTimeout(() => {
-              a = b = -1;
-            }, 1000);
-          }
-        } else {
-          b = -1;
-          a = i;
-        }
-      }}
-    />
-  {/each}
+    {#each grid as emoji, i}
+        <Square
+            {emoji}
+            selected={a === i || b === i}
+            found={found.includes(emoji)}
+            group={grid.indexOf(emoji) === i ? "a" : "b"}
+            on:click={() => {
+                clearTimeout(reset_timeout);
+                if (a === -1 && b === -1) {
+                    a = i;
+                } else if (b === -1) {
+                    b = i;
+                    if (grid[a] === grid[b]) {
+                        dispatch("found", { emoji });
+                    } else {
+                        reset_timeout = setTimeout(() => {
+                            a = b = -1;
+                        }, 1000);
+                    }
+                } else {
+                    b = -1;
+                    a = i;
+                }
+            }}
+        />
+    {/each}
 </div>
 
 <style>
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: repeat(4, 1fr);
-    grid-gap: 0.5em;
-    height: 100%;
-  }
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(var(--size), 1fr);
+        grid-template-rows: repeat(var(--size), 1fr);
+        grid-gap: 0.5em;
+        height: 100%;
+        perspective: 100vw;
+    }
 </style>
